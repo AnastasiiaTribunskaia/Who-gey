856721d8225208d3e82272538aeec308f5a82fbf
Anastasiia Tribunskaia <nasta-kuz91@mail.ru>
2022-08-28 15:10:14 +0300

    Сделали программу Эдик-педик

--- Who_gey/Program.cs
+++ Who_gey/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Эдик")
+{
+    Console.WriteLine("Эдик- педик");
+}
+    else
+        if(username == "Рома")
+        {
+            Console.WriteLine("Рома - почти педик");
+        }
+        else
+        {
+        Console.Write(username);
+        Console.WriteLine(" - не педик");
+        }
